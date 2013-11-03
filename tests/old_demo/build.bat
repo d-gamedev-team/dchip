3638509fbc82c61743c931932c625bbc979388a9
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2013-11-03 18:51:28 +0100

    Force build test.

--- tests/old_demo/build.bat
+++ tests/old_demo/build.bat
@@ -5,4 +5,4 @@ set CHIP_ALLOW_PRIVATE_ACCESS=-version=CHIP_ALLOW_PRIVATE_ACCESS
 
 rem Note: -O seems to be broken
 rem set OPTIMIZE_FLAGS=-release -inline -noboundscheck
-rdmd -I..\..\src %OPTIMIZE_FLAGS% %CHIP_ALLOW_PRIVATE_ACCESS% main.d
+rdmd --force -I..\..\src %OPTIMIZE_FLAGS% %CHIP_ALLOW_PRIVATE_ACCESS% main.d
